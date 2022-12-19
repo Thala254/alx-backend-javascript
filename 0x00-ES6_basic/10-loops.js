d8ae67676c9c8b26481fde0f4b19f146e065a5f3
thala254 <thalamine@gmail.com>
2022-12-20 00:45:05 +0300

    added files

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let newArray = [];
+  for (let element of array) {
+    newArray.push(`${appendString}${element}`);
+  }
+
+  return newArray;
+}
