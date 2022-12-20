ac9223a1ce6909e00e465632b7ef59e8748cd999
thala254 <thalamine@gmail.com>
2022-12-21 01:47:54 +0300

    corrections

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => ([{
-      status: result.status,
-      value: result,
-    }]));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+
+  const fileData = await uploadPhoto(fileName).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
+
+  return Promise.resolve([userData, fileData]);
 }
